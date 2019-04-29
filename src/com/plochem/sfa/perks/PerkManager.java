c4f80e8ab3f871b07a0090ee6b00d1710f97a1b8
Ryan Chu <plochemisswag@gmail.com>
2019-04-28 23:06:26 -0700

    Update PerkManager.java

--- src/com/plochem/sfa/perks/PerkManager.java
+++ src/com/plochem/sfa/perks/PerkManager.java
@@ -1,11 +1,19 @@
 package com.plochem.sfa.perks;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
 public class PerkManager {
-	{
-//		for(PerkType type : PerkType.values()) {
-//			if(playerData.get(type.toString()) == null) {
-//				playerData.set(type.toString(), 0);
-//			}
-//		}
+	public static int PURCHASE_SCALE_FACTOR = 2;
+	
+	public static int currentLevel(PerkType type, Player p) {
+		File playerFile = new File("plugins/SFA/playerPerks/" + p.getUniqueId().toString() + ".yml");
+		YamlConfiguration playerData = YamlConfiguration.loadConfiguration(playerFile);
+		if(playerData.get(type.toString()) == null) {
+			return 0;
+		}
+		return (Integer)playerData.get(type.toString());
 	}
 }
