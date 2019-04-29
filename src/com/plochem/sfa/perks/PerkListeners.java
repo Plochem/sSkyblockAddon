75dd716aa9528df93b60689e43ae18b2b8b52ca7
Ryan Chu <plochemisswag@gmail.com>
2019-04-28 19:03:58 -0700

    Create PerkListeners.java

---
+++ src/com/plochem/sfa/perks/PerkListeners.java
@@ -0,0 +1,13 @@
+package com.plochem.sfa.perks;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PerkListeners implements Listener{
+	@EventHandler
+	public void onKill(PlayerDeathEvent e) {
+		
+	}
+	
+}
