3f927639180a6a428f082c9831ed03b83309c611
Ryan Chu <plochemisswag@gmail.com>
2019-04-28 19:04:33 -0700

    Create GeneratorType.java

---
+++ src/com/plochem/sfa/generator/GeneratorType.java
@@ -0,0 +1,32 @@
+package com.plochem.sfa.generator;
+
+public enum GeneratorType {
+	COAL(30, 500000, "§8"),
+	REDSTONE(30, 500000, "§4"),
+	IRON_INGOT(40, 650000, "§f"),
+	GOLD_INGOT(40, 650000, "§e"),
+	EMERALD(50, 800000, "§a"),
+	DIAMOND(60, 1000000, "§b");
+	
+	private int interval;
+	private int cost;
+	private String color;
+	
+	private GeneratorType(int interval, int cost, String color) {
+		this.interval = interval;
+		this.cost = cost;
+		this.color = color;
+	}
+	
+	public int getInterval() {
+		return interval;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+}
