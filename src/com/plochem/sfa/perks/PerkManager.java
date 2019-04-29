b429d48e08bf0975b5deefa14612c190b8c8af2a
Ryan Chu <plochemisswag@gmail.com>
2019-04-28 19:03:54 -0700

    Create PerkManager.java

---
+++ src/com/plochem/sfa/perks/PerkManager.java
@@ -0,0 +1,11 @@
+package com.plochem.sfa.perks;
+
+public class PerkManager {
+	{
+//		for(PerkType type : PerkType.values()) {
+//			if(playerData.get(type.toString()) == null) {
+//				playerData.set(type.toString(), 0);
+//			}
+//		}
+	}
+}
