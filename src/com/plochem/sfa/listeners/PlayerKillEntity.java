e9f714bcf3020efd2aef4ed7ef4a3963559076d4
Ryan Chu <plochemisswag@gmail.com>
2019-05-18 23:17:06 -0700

    added rewards if shot mob/player

--- src/com/plochem/sfa/listeners/PlayerKillEntity.java
+++ src/com/plochem/sfa/listeners/PlayerKillEntity.java
@@ -3,6 +3,7 @@ package com.plochem.sfa.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -18,31 +19,39 @@ public class PlayerKillEntity implements Listener{
 		Entity entity = e.getEntity();
 		if(entity.getLastDamageCause() instanceof EntityDamageByEntityEvent){
 			EntityDamageByEntityEvent nEvent = (EntityDamageByEntityEvent) entity.getLastDamageCause();
-			if(nEvent.getDamager() instanceof Player) { // Player kills mob or player
-				Player killer = (Player) nEvent.getDamager();
-				int baseMoney = 2;
-				int baseXP = e.getDroppedExp();
-				String boosterMessage = "";
-				String expMessage = "";
-				String killedWhoMessage = " (Killed a mob)";
-				e.setDroppedExp(0);
-				if(entity instanceof Player) {
-					baseMoney = 5;
-					killedWhoMessage = " (Killed a player)";
+			Player killer;
+			if(nEvent.getDamager() instanceof Projectile) {
+				if(((Projectile) nEvent.getDamager()).getShooter() instanceof Player){
+					killer = (Player)(((Projectile)nEvent.getDamager()).getShooter());
+				} else {
+					return;
 				}
-				if(BoosterManager.moneyBoosterIsActive()) {
-					baseMoney *= 2;
-					boosterMessage = " §a(x2 money booster)";
-				}
-				if(BoosterManager.expBoosterIsActive()) {
-					baseXP *= 2;
-					expMessage = " §3(x2 EXP booster)";
-				}
-				Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "xp " + (int)(killer.getExp() + baseXP) + " " + killer.getName());
-				sfa.getSEconomy().getEconomyImplementer().depositPlayer(killer, baseMoney);
-				killer.sendMessage("§a+$" + baseMoney + killedWhoMessage + boosterMessage);
-				killer.sendMessage("§3+" + baseXP + " exp" + killedWhoMessage + expMessage);
+			} else {
+				killer = (Player)nEvent.getDamager();
 			}
+			int baseMoney = 2;
+			int baseXP = e.getDroppedExp();
+			String boosterMessage = "";
+			String expMessage = "";
+			String killedWhoMessage = " (Killed a mob)";
+			e.setDroppedExp(0);
+			if(entity instanceof Player) {
+				baseMoney = 5;
+				killedWhoMessage = " (Killed a player)";
+			}
+			if(BoosterManager.moneyBoosterIsActive()) {
+				baseMoney *= 2;
+				boosterMessage = " §a(x2 money booster)";
+			}
+			if(BoosterManager.expBoosterIsActive()) {
+				baseXP *= 2;
+				expMessage = " §3(x2 EXP booster)";
+			}
+			Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "xp " + (int)(killer.getExp() + baseXP) + " " + killer.getName());
+			sfa.getSEconomy().getEconomyImplementer().depositPlayer(killer, baseMoney);
+			killer.sendMessage("§a+$" + baseMoney + killedWhoMessage + boosterMessage);
+			killer.sendMessage("§3+" + baseXP + " exp" + killedWhoMessage + expMessage);
+
 		}
 	}
 
