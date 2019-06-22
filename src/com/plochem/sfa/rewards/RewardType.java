a1f05631d75616ddb02a99f24c6feae0fca1665d
Ryan Chu <plochemisswag@gmail.com>
2019-06-21 23:03:00 -0700

    money and xp for each reward type

--- src/com/plochem/sfa/rewards/RewardType.java
+++ src/com/plochem/sfa/rewards/RewardType.java
@@ -1,10 +1,34 @@
 package com.plochem.sfa.rewards;
 
 public enum RewardType {
-	DAILY,
-	MONTHLY,
-	ELITEMONTHLY,
-	MASTERMONTHLY,
-	LEGENDMONTHLY,
-	MYSTICMONTHLY;
+	DAILY(1,1),
+	MONTHLY(1,1),
+	ELITEMONTHLY(1,1),
+	MASTERMONTHLY(1,1),
+	LEGENDMONTHLY(1,1),
+	MYSTICMONTHLY(1,1);
+	
+	private int money;
+	private int xp;
+	
+	private RewardType(int money, int xp) {
+		this.setMoney(money);
+		this.setXp(xp);
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public int getXp() {
+		return xp;
+	}
+
+	public void setXp(int xp) {
+		this.xp = xp;
+	}
 }
