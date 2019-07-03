2121d936bc20a0a97c6ce186ff38537970f87415
Ryan Chu <plochemisswag@gmail.com>
2019-07-02 23:32:04 -0700

    ore generator in cobble gen

---
+++ src/com/plochem/ssa/oregen/OreGenListener.java
@@ -0,0 +1,69 @@
+package com.plochem.ssa.oregen;
+
+import java.util.Random;
+import java.util.UUID;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
+
+import com.wasteofplastic.askyblock.ASkyBlockAPI;
+
+public class OreGenListener implements Listener{
+
+	@EventHandler
+	public void onForm(BlockFromToEvent e) {
+		Material type = e.getBlock().getType();
+		if (type == Material.WATER || type == Material.STATIONARY_WATER || type == Material.LAVA || type == Material.STATIONARY_LAVA){
+			Block b = e.getToBlock();
+			if (b.getType() == Material.AIR){
+				if (generatesCobble(type, b)){
+					//UUID owner = ASkyBlockAPI.getInstance().getIslandAt(e.getBlock().getLocation()).getOwner();
+					int r = new Random().nextInt(100) + 1; // 1- 100
+					System.out.println(r);
+					if(r == 1) { // 1
+						e.getToBlock().setType(Material.EMERALD_ORE);
+					} else if (r <= 4) { // 3
+						e.getToBlock().setType(Material.DIAMOND_ORE);
+					} else if(r <= 9) { // 5
+						e.getToBlock().setType(Material.GOLD_ORE);
+					} else if(r <= 14) { // 5
+						e.getToBlock().setType(Material.IRON_ORE);
+					} else if(r <= 24) { // 10
+						e.getToBlock().setType(Material.LAPIS_ORE);
+					} else if(r <= 34) { // 10
+						e.getToBlock().setType(Material.REDSTONE_ORE);
+					} else if(r <= 49) { // 15
+						e.getToBlock().setType(Material.COAL_ORE);
+					}
+				}
+			}
+		}
+	}
+
+	private final BlockFace[] faces = new BlockFace[]{
+			BlockFace.SELF,
+			BlockFace.UP,
+			BlockFace.DOWN,
+			BlockFace.NORTH,
+			BlockFace.EAST,
+			BlockFace.SOUTH,
+			BlockFace.WEST
+	};
+
+	public boolean generatesCobble(Material type, Block b){
+		Material mirrorID1 = (type == Material.WATER || type == Material.STATIONARY_WATER ? Material.LAVA : Material.WATER);
+		Material mirrorID2 = (type == Material.WATER || type == Material.STATIONARY_WATER ? Material.STATIONARY_LAVA : Material.STATIONARY_WATER);
+		for (BlockFace face : faces){
+			Block r = b.getRelative(face, 1);
+			if (r.getType() == mirrorID1 || r.getType() == mirrorID2){
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
