9fc3a22711ca92b4db755b59bac6e39b35552afa
Ryan Chu <plochemisswag@gmail.com>
2019-07-02 23:31:42 -0700

    island level prefix

--- src/com/plochem/ssa/listeners/ChatHandling.java
+++ src/com/plochem/ssa/listeners/ChatHandling.java
@@ -6,6 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import com.plochem.ssa.SSkyblockAddon;
+import com.wasteofplastic.askyblock.ASkyBlockAPI;
 
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 
@@ -20,7 +21,8 @@ public class ChatHandling implements Listener{
 	public void onChat(AsyncPlayerChatEvent e) {
 		Player p = e.getPlayer();
 		String rank = PermissionsEx.getUser(p).getPrefix().replaceAll("&", "§");
-		e.setFormat(rank + p.getName() + "§r: " + e.getMessage().replaceAll("%", "%%"));
+		long islandLevel = ASkyBlockAPI.getInstance().getLongIslandLevel((p.getUniqueId()));
+		e.setFormat("§7[§a" + islandLevel + "§7]" + rank + p.getName() + "§r: " + e.getMessage().replaceAll("%", "%%"));
 	}
 
 }
