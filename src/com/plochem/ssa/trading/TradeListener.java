dbd674a749fb50cd44b11a5941b21a390c954301
Ryan Chu <plochemisswag@gmail.com>
2019-07-07 22:58:56 -0700

    reset confirmation on change

--- src/com/plochem/ssa/trading/TradeListener.java
+++ src/com/plochem/ssa/trading/TradeListener.java
@@ -3,49 +3,70 @@ package com.plochem.ssa.trading;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class TradeListener implements Listener{
-	int[] accepterTradeSpots = {5,6,7,8,
+	private int[] accepterTradeSpots = {5,6,7,8,
 			14,15,16,17,
 			23,24,25,26,
 			32,33,34,35,
 			41,42,43,44};
-	int[] accepterConfirm = {50,51,52,53};
+	private int[] accepterConfirm = {50,51,52,53};
 	
-	int[] requesterTradeSpots = {0,1,2,3,
+	private int[] requesterTradeSpots = {0,1,2,3,
 			9,10,11,12,
 			18,19,20,21,
 			27,28,29,30,
 			36,37,38,39};
-	int[] requesterConfirm = {45,46,47,48};
+	private int[] requesterConfirm = {45,46,47,48};
 	@EventHandler
 	public void onclick(InventoryClickEvent e) {
 		Player clicker = (Player)e.getWhoClicked();
+		
 		if(isRequester(clicker) || isAccepter(clicker)){
 			if(e.getClickedInventory() != null && e.getClickedInventory().getItem(e.getSlot()) != null) {
 				if(e.getInventory().getTitle().contains("trade")) {
 					e.setCancelled(true);
 					int[] tradeSpots = requesterTradeSpots;
+					int[] confirmSpots = requesterConfirm;
 					if(isAccepter(clicker)) {
 						tradeSpots = accepterTradeSpots;
+						confirmSpots = accepterConfirm;
 					}
+					ItemStack confirm = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short)14);
+					ItemMeta meta = confirm.getItemMeta();
+					meta.setDisplayName("§c§lNOT READY");
+					confirm.setItemMeta(meta);
 					if(e.getClickedInventory().getTitle().contains("trade")) { // reclaim
 						if(isInArray(e.getSlot(), tradeSpots)) {
 							clicker.getInventory().addItem(e.getCurrentItem());
 							e.getClickedInventory().setItem(e.getSlot(), null);
+						} else if(isInArray(e.getSlot(), confirmSpots)) {
+							if(e.getCurrentItem().getDurability() == (short)14) {
+								confirm = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short)5);
+								meta = confirm.getItemMeta();
+								meta.setDisplayName("§a§lREADY");
+								confirm.setItemMeta(meta);
+							}
+							for(int i : confirmSpots) {
+								e.getInventory().setItem(i, confirm);
+							}
 						}
-					}
-					if(e.getClickedInventory().getType() == InventoryType.PLAYER) { // clicked own inventory
+					} else if(e.getClickedInventory().getType() == InventoryType.PLAYER) { // clicked own inventory & add item
 						for(int pos : tradeSpots) {
 							if(e.getInventory().getItem(pos) == null) { // available spot
 								e.getInventory().setItem(pos, e.getCurrentItem());
 								e.getClickedInventory().setItem(e.getSlot(), null);
+								for(int i : requesterConfirm) e.getInventory().setItem(i, confirm);
+								for(int i : accepterConfirm) e.getInventory().setItem(i, confirm);
 								break;
 							}									
 						}
