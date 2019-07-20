ef6e2389b3ba6b6bfa5722868e2cf1dfaaabfbd9
Ryan Chu <plochemisswag@gmail.com>
2019-07-19 23:53:56 -0700

    crate keys for rewards

--- src/com/plochem/ssa/rewards/RewardType.java
+++ src/com/plochem/ssa/rewards/RewardType.java
@@ -1,19 +1,21 @@
 package com.plochem.ssa.rewards;
 
 public enum RewardType {
-	DAILY(200,300),
-	MONTHLY(5000,3000),
-	ELITE_MONTHLY(5000,3000),
-	MASTER_MONTHLY(5000,3000),
-	LEGEND_MONTHLY(5000,3000),
-	MYSTIC_MONTHLY(5000,3000);
+	DAILY(200,300, "Rare"),
+	MONTHLY(5000,3000, "Straxidus"),
+	ELITE_MONTHLY(5000,3000, "Destiny"),
+	MASTER_MONTHLY(5000,3000, "Destiny"),
+	LEGEND_MONTHLY(5000,3000, "Straxidus"),
+	MYSTIC_MONTHLY(5000,3000, "Straxidus");
 	
 	private int money;
 	private int xp;
+	private String crateKey;
 	
-	private RewardType(int money, int xp) {
+	private RewardType(int money, int xp, String crateKey) {
 		this.setMoney(money);
 		this.setXp(xp);
+		this.crateKey = crateKey;
 	}
 
 	public int getMoney() {
@@ -30,5 +32,9 @@ public enum RewardType {
 
 	public void setXp(int xp) {
 		this.xp = xp;
+	}
+	
+	public String getCrateKey() {
+		return crateKey;
 	}
 }
