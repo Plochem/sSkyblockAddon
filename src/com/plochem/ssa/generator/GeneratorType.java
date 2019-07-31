08f35e4211dc186664aeeb09cf304a4249aab64f
Ryan Chu <plochemisswag@gmail.com>
2019-07-31 00:56:40 -0700

    changed spawnrate

--- src/com/plochem/ssa/generator/GeneratorType.java
+++ src/com/plochem/ssa/generator/GeneratorType.java
@@ -1,12 +1,12 @@
 package com.plochem.ssa.generator;
 
 public enum GeneratorType {
-	COAL(30, 250000, "§8", 500),
-	REDSTONE(30, 300000, "§4", 500),
-	IRON_INGOT(40, 400000, "§f", 500),
-	GOLD_INGOT(40, 450000, "§e", 500),
-	DIAMOND(50, 550000, "§b", 500),
-	EMERALD(60, 600000, "§a", 500);
+	COAL(3, 250000, "§8", 7500),
+	REDSTONE(4, 300000, "§4", 7500),
+	IRON_INGOT(7, 400000, "§f", 7500),
+	GOLD_INGOT(9, 450000, "§e", 7500),
+	DIAMOND(11, 550000, "§b", 7500),
+	EMERALD(15, 600000, "§a", 7500);
 	
 	private int interval; //sec
 	private int cost;
