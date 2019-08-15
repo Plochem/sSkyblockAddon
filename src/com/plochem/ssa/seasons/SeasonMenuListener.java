f23fb2e30f76fcaeb7b7d92f7d069882cc204dbe
Ryan Chu <plochemisswag@gmail.com>
2019-08-14 23:51:40 -0700

    small changes and bugfixes

--- src/com/plochem/ssa/seasons/SeasonMenuListener.java
+++ src/com/plochem/ssa/seasons/SeasonMenuListener.java
@@ -2,6 +2,7 @@ package com.plochem.ssa.seasons;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +15,7 @@ public class SeasonMenuListener implements Listener{
 	 * #1 - $30 Buycraft, IslandTop#1 Tag &  Special Island
 #2 - $20 Buycraft & Special Island
 #3 - $10 Buycraft & Special Island
-#4 to #10 -  $1 Buycraft
+#4 to #10 -  
 	 */
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent e) {
@@ -22,36 +23,43 @@ public class SeasonMenuListener implements Listener{
 			if(e.getInventory().getTitle().equalsIgnoreCase("Season Rewards")) {
 				e.setCancelled(true);
 				if(e.getClickedInventory().getTitle().equalsIgnoreCase("Season Rewards")) {
-					Player p = (Player)e.getWhoClicked();
-					ItemStack item = e.getCurrentItem();
-					String reward = ChatColor.stripColor(item.getItemMeta().getDisplayName());
-					int seasonRewardIsIn = Integer.parseInt(reward.split(" ")[1]);
-					int rank = -1;
-					
-					if(SeasonManager.getCurrentSeason() > seasonRewardIsIn) { // check if attempting the claim previous season rewards
-						rank = SeasonManager.getRankInSeason(seasonRewardIsIn, p);
-						if(rank !=0 && rank <= SeasonManager.getHighestRank(seasonRewardIsIn, reward)) {
-							if(rank >= SeasonManager.getLeastRank(seasonRewardIsIn, reward)) { // in the rank range
-								if(SeasonManager.alreadyClaimed(seasonRewardIsIn, reward, p)) {
-									p.sendMessage("§cYou already claimed this reward!");
-								} else {
-									for(String cmd : SeasonManager.getCommands(seasonRewardIsIn, reward)) {
-										Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replaceAll("%player%", p.getName()));
-									}
-									p.sendMessage(SeasonManager.getClaimMsg(seasonRewardIsIn, reward).replaceAll("%player%", p.getName()));
-									SeasonManager.claimed(seasonRewardIsIn, reward, p);
-								}
-							} else {
-								p.sendMessage("§cYou are qualified for a better reward!");
+					if(e.getCurrentItem().getType() != Material.STAINED_GLASS_PANE && e.getCurrentItem().getType() != Material.BEACON) {
+						Player p = (Player)e.getWhoClicked();
+						ItemStack item = e.getCurrentItem();
+						String reward = ChatColor.stripColor(item.getItemMeta().getDisplayName());
+						int seasonRewardIsIn = Integer.parseInt(reward.split(" ")[1]);
+						int rank = -1;
+
+						if(SeasonManager.getCurrentSeason() > seasonRewardIsIn) { // check if attempting the claim previous season rewards
+							rank = SeasonManager.getRankInSeason(seasonRewardIsIn, p);
+							if(rank ==0) {
+								p.sendMessage("§cYou did not participate in this season.");
+								p.closeInventory();
+								return;
 							}
-							
+							if(rank <= SeasonManager.getHighestRank(seasonRewardIsIn, reward)) {
+								if(rank >= SeasonManager.getLeastRank(seasonRewardIsIn, reward)) { // in the rank range
+									if(SeasonManager.alreadyClaimed(seasonRewardIsIn, reward, p)) {
+										p.sendMessage("§cYou already claimed this reward!");
+									} else {
+										for(String cmd : SeasonManager.getCommands(seasonRewardIsIn, reward)) {
+											Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replaceAll("%player%", p.getName()));
+										}
+										p.sendMessage(SeasonManager.getClaimMsg(seasonRewardIsIn, reward).replaceAll("%player%", p.getName()));
+										SeasonManager.claimed(seasonRewardIsIn, reward, p);
+									}
+								} else {
+									p.sendMessage("§cYou are qualified for a better reward!");
+								}
+
+							} else {
+								p.sendMessage("§cYour island rank of §b#" + rank + " §cin season " + seasonRewardIsIn + " is not high enough to qualify for this reward.");
+							}
 						} else {
-							p.sendMessage("§cYour island rank in season " + seasonRewardIsIn + " is not qualified to claim this reward.");
+							p.sendMessage("§cYou may not claim the rewards for season " + seasonRewardIsIn + " yet.");
 						}
-					} else {
-						p.sendMessage("§cYou may not claim the rewards for season " + seasonRewardIsIn + " yet.");
+						p.closeInventory();
 					}
-	
 				}
 			}
 		}
