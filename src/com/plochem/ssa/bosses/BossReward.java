41a7882a564192276870bd766666484a8de82baf
Ryan Chu <plochemisswag@gmail.com>
2020-01-13 23:17:36 -0800

    commands for rewards

--- src/com/plochem/ssa/bosses/BossReward.java
+++ src/com/plochem/ssa/bosses/BossReward.java
@@ -1,5 +1,22 @@
 package com.plochem.ssa.bosses;
 
-public class BossReward {
+import java.util.List;
 
+public class BossReward {
+	private List<List<String>> topDmgCommands;
+	private List<String> otherDmgCommands;
+	
+	public BossReward(List<List<String>> topDmgCommands, List<String> otherDmgCommands) {
+		this.topDmgCommands = topDmgCommands;
+		this.otherDmgCommands = otherDmgCommands;
+	}
+	
+	public List<List<String>> getTopDmgCommands() {
+		return topDmgCommands;
+	}
+	
+	public List<String> getOtherDmgCommands() {
+		return otherDmgCommands;
+	}
+	
 }
