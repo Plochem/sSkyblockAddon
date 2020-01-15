2e6a79a9924c9bfd828a631b681833e00a78b86b
Ryan Chu <plochemisswag@gmail.com>
2020-01-14 22:48:00 -0800

    new skill

---
+++ src/com/plochem/ssa/bosses/skills/special/Cobwebs.java
@@ -0,0 +1,43 @@
+package com.plochem.ssa.bosses.skills.special;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.plochem.ssa.SSkyblockAddon;
+import com.plochem.ssa.bosses.BossEntity;
+import com.plochem.ssa.bosses.skills.Skill;
+
+public class Cobwebs extends Skill{
+
+	public Cobwebs() {
+		super("Cobwebs", 10, Arrays.asList("§7Spawns cobwebs under 2 random players", "§7within a 10 block radius"));
+	}
+
+	@Override
+	public void cast(BossEntity boss, List<UUID> nearby) {
+		Collections.shuffle(nearby);	
+		for(int i = 0; i < Math.min(2, nearby.size()); i++) {
+			Player p = Bukkit.getPlayer(nearby.get(i));
+			Location loc = p.getLocation();
+			loc.getBlock().setType(Material.WEB);
+			p.teleport(loc);
+			new BukkitRunnable() {	
+				Location temp = loc;
+				@Override
+				public void run() {
+					temp.getBlock().setType(Material.AIR);
+				}
+			}.runTaskLater(SSkyblockAddon.getPlugin(SSkyblockAddon.class), 20*8);
+		}
+
+	}
+
+}
