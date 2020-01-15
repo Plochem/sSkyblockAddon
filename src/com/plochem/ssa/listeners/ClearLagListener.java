dddef91e60b65e76ef15c09ce1f25d859d3d3371
Ryan Chu <plochemisswag@gmail.com>
2020-01-14 22:47:45 -0800

    doesn't remove boss now

--- src/com/plochem/ssa/listeners/ClearLagListener.java
+++ src/com/plochem/ssa/listeners/ClearLagListener.java
@@ -1,13 +1,19 @@
 package com.plochem.ssa.listeners;
 
+import java.util.Iterator;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 
-import com.kiwifisher.mobstacker.MobStacker;
+import com.kiwifisher.mobstacker.utils.StackUtils;
+import com.plochem.ssa.bosses.BossEntity;
+import com.plochem.ssa.bosses.BossManager;
 
 import me.minebuilders.clearlag.events.EntityRemoveEvent;
 
@@ -15,7 +21,7 @@ public class ClearLagListener implements Listener{
 	@EventHandler
 	public void onClear(ServerCommandEvent e) {
 		if(e.getCommand().equals("/lagg clear")) {
-			MobStacker.getPlugin(MobStacker.class).removeAllStacks();
+			removeAllStacks();
 			Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "All stacks were successfully removed");
 		}
 	}
@@ -23,14 +29,31 @@ public class ClearLagListener implements Listener{
 	@EventHandler
 	public void onClear(PlayerCommandPreprocessEvent e) {
 		if(e.getMessage().equals("/lagg clear")) {
-			MobStacker.getPlugin(MobStacker.class).removeAllStacks();
+			removeAllStacks();
 			Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "All stacks were successfully removed");
 		}
 	}
 	
 	@EventHandler
 	public void onClear(EntityRemoveEvent e) {
-		MobStacker.getPlugin(MobStacker.class).removeAllStacks();
+		removeAllStacks();
 	}
 
+	
+	private void removeAllStacks() {
+        for(Iterator<World> i = Bukkit.getServer().getWorlds().iterator(); i.hasNext();)
+        {
+            World world = i.next();
+            Iterator<LivingEntity> i2 = world.getLivingEntities().iterator();
+            while(i2.hasNext()) 
+            {
+                LivingEntity entity = i2.next();
+    			BossEntity hitBoss = BossManager.getCurrBosses().get(entity.getUniqueId());
+    			if(hitBoss == null) { // not boss
+                    if(StackUtils.hasRequiredData(entity))
+                        entity.remove();
+    			}
+            }
+        }
+	}
 }
