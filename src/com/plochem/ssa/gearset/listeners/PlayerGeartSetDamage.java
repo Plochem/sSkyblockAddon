249c7e7b051eaf78bc21be0bec8adac126fdabd4
Ryan Chu <plochemisswag@gmail.com>
2020-01-17 00:16:32 -0800

    Create PlayerGeartSetDamage.java

---
+++ src/com/plochem/ssa/gearset/listeners/PlayerGeartSetDamage.java
@@ -0,0 +1,24 @@
+package com.plochem.ssa.gearset.listeners;
+
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+import com.plochem.ssa.gearset.GearSet;
+import com.plochem.ssa.gearset.GearSetManager;
+
+public class PlayerGeartSetDamage implements Listener{
+	@EventHandler
+	public void onDamage(ProjectileHitEvent e) {
+		if(e.getEntity() instanceof Arrow && e.getEntity().getShooter() instanceof Player) {
+			Player shooter = (Player)e.getEntity().getShooter();
+			GearSet set = GearSetManager.getGearSet("Paladin");
+			if(GearSetManager.isWearingSet(set, shooter)) {
+				set.getAbility().execute();
+			}
+		}
+		
+	}
+}
