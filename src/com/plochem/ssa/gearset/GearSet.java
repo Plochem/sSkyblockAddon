e564a88a8fbf6f22dd50001d681b8a110c52341e
Ryan Chu <plochemisswag@gmail.com>
2020-01-18 23:57:22 -0800

    separate armor and items

--- src/com/plochem/ssa/gearset/GearSet.java
+++ src/com/plochem/ssa/gearset/GearSet.java
@@ -6,16 +6,22 @@ import java.util.List;
 import org.bukkit.inventory.ItemStack;
 
 public class GearSet {
+	private List<ItemStack> armor = new ArrayList<>();
 	private List<ItemStack> items = new ArrayList<>();
 	private String name;
 	private Ability ability;
 	
-	public GearSet(List<ItemStack> items, String name, Ability ability) {
+	public GearSet(List<ItemStack> armor, List<ItemStack> items, String name, Ability ability) {
+		this.armor = armor;
 		this.items = items;
 		this.name = name;
 		this.ability = ability;
 	}
 
+	public List<ItemStack> getArmor() {
+		return armor;
+	}
+	
 	public List<ItemStack> getItems() {
 		return items;
 	}
