0faffebc1eeb64849774f633f4388e90842fe401
Ryan Chu <plochemisswag@gmail.com>
2020-01-20 22:36:08 -0800

    add the paladin gearset

--- src/com/plochem/ssa/gearset/GearSetManager.java
+++ src/com/plochem/ssa/gearset/GearSetManager.java
@@ -1,32 +1,50 @@
 package com.plochem.ssa.gearset;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import com.plochem.ssa.ItemBuilder;
+import com.plochem.ssa.bosses.Tier;
+import com.plochem.ssa.gearset.abilities.StickyBow;
 
 public class GearSetManager {
-	private static List<GearSet> gearsets = new ArrayList<>();
+	private static Map<String, GearSet> gearsets = new HashMap<>();
 	static {
-		gearsets.add(new GearSet(items, name, ability)) //TODO itembuilder with NBT methods
+		gearsets.put("Paladin", new GearSet(Arrays.asList(
+				new ItemBuilder(Material.DIAMOND_HELMET, 1).displayname("§5§lPaladin §d§lHelmet").enchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10).enchant(Enchantment.DURABILITY, 10).setNBTString("gearset", "paladin_helmet").build(),
+				new ItemBuilder(Material.DIAMOND_CHESTPLATE, 1).displayname("§5§lPaladin §d§lChestplate").enchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10).enchant(Enchantment.DURABILITY, 10).setNBTString("gearset", "paladin_chestplate").build(), 
+				new ItemBuilder(Material.DIAMOND_LEGGINGS, 1).displayname("§5§lPaladin §d§lLeggings").enchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10).enchant(Enchantment.DURABILITY, 10).setNBTString("gearset", "paladin_leggings").build(),
+				new ItemBuilder(Material.DIAMOND_BOOTS, 1).displayname("§5§lPaladin §d§lBoots").enchant(Enchantment.PROTECTION_ENVIRONMENTAL, 10).enchant(Enchantment.DURABILITY, 10).setNBTString("gearset", "paladin_boots").build(), 
+				new ItemBuilder(Material.BOW, 1).displayname("§5§lPaladin §d§lBow").enchant(Enchantment.ARROW_DAMAGE, 6).enchant(Enchantment.ARROW_FIRE, 2).enchant(Enchantment.ARROW_INFINITE, 1).setNBTString("gearset", "paladin_bow").build()),
+				Tier.EPIC, "Paladin", new StickyBow())); 
 	}
+	
 	public static boolean isWearingSet(GearSet set, Player p) {
-		for(ItemStack i : set.getItems()) {
-			if(!p.getInventory().contains(i)) {
-				
+		for(ItemStack item : set.getItems()) {
+			if(!invContains(p.getInventory(), item)) {
+				return false;
 			}
 		}
-		// loop inventory. check if contains set.getITemStacks
-		return false;
+		return true;
 	}
 	
 	public static GearSet getGearSet(String s) {
-		for(GearSet set : gearsets) {
-			if(set.getName().equals(s)) {
-				return set;
+		return gearsets.get(s);
+	}
+	
+	private static boolean invContains(PlayerInventory inv, ItemStack gearItem) {
+		for(ItemStack armor : inv.getContents()) {
+			if(new ItemBuilder(gearItem).getNBTString("gearset").equals(new ItemBuilder(armor).getNBTString("gearset"))) {
+				return true;
 			}
 		}
-		return null;
+		return false;
 	}
 }
