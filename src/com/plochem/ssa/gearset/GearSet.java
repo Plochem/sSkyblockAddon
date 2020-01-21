4e8f7d4b2b4a2a70bbbec38789a650680db26e5a
Ryan Chu <plochemisswag@gmail.com>
2020-01-20 22:36:25 -0800

    removed armor list. added tiers

--- src/com/plochem/ssa/gearset/GearSet.java
+++ src/com/plochem/ssa/gearset/GearSet.java
@@ -5,25 +5,27 @@ import java.util.List;
 
 import org.bukkit.inventory.ItemStack;
 
+import com.plochem.ssa.bosses.Tier;
+
 public class GearSet {
-	private List<ItemStack> armor = new ArrayList<>();
 	private List<ItemStack> items = new ArrayList<>();
+	private Tier tier;
 	private String name;
 	private Ability ability;
 	
-	public GearSet(List<ItemStack> armor, List<ItemStack> items, String name, Ability ability) {
-		this.armor = armor;
+	public GearSet(List<ItemStack> items, Tier tier, String name, Ability ability) {
 		this.items = items;
+		this.tier = tier;
 		this.name = name;
 		this.ability = ability;
-	}
-
-	public List<ItemStack> getArmor() {
-		return armor;
 	}
 	
 	public List<ItemStack> getItems() {
 		return items;
+	}
+	
+	public Tier getTier() {
+		return tier;
 	}
 
 	public String getName() {
