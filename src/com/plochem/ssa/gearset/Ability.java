2b33d81a5efd98e11ba4a55320d3df57b1fbd2e5
Ryan Chu <plochemisswag@gmail.com>
2020-01-20 22:36:50 -0800

    changed to abstract

--- src/com/plochem/ssa/gearset/Ability.java
+++ src/com/plochem/ssa/gearset/Ability.java
@@ -1,5 +1,32 @@
 package com.plochem.ssa.gearset;
 
-public interface Ability {
-	void execute();
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+
+public abstract class Ability {
+	private String name;
+	private List<String> description;
+	private int chance;
+	
+	public Ability(String name, int chance, List<String> description) {
+		this.name = name;
+		this.description = description;
+		this.chance = chance;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public List<String> getDescription(){
+		return description;
+	}
+	
+	public int getChance() {
+		return chance;
+	}
+	
+	public abstract void execute(Player caster, List<UUID> targets);
 }
