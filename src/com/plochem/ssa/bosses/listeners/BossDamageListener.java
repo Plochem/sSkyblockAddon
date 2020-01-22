c208a289e25c3f32fb0f4a9d5a3a3bd30c1c0f5e
Ryan Chu <plochemisswag@gmail.com>
2020-01-22 00:04:43 -0800

    accounts for damaging with bow

--- src/com/plochem/ssa/bosses/listeners/BossDamageListener.java
+++ src/com/plochem/ssa/bosses/listeners/BossDamageListener.java
@@ -1,7 +1,9 @@
 package com.plochem.ssa.bosses.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -15,18 +17,30 @@ import com.plochem.ssa.bosses.BossManager;
 public class BossDamageListener implements Listener{
 	@EventHandler
 	public void onHit(EntityDamageByEntityEvent e) {
-		if(e.getDamager() instanceof Player) {
-			BossEntity hitBoss = BossManager.getCurrBosses().get(e.getEntity().getUniqueId());
-			if(hitBoss != null) {
-				e.setDamage(e.getDamage() - hitBoss.getStats().getDefense());
-				Double dmg = hitBoss.getPlayerDamage().get(e.getDamager().getUniqueId());
-				if(dmg == null) dmg = 0.0;
-				hitBoss.getPlayerDamage().put(e.getDamager().getUniqueId(), dmg + e.getDamage());
-				hitBoss.setNameAndHealth();
+		Entity entity = e.getEntity();
+		BossEntity hitBoss = BossManager.getCurrBosses().get(entity.getUniqueId());
+		if(hitBoss != null) {
+			Player damager;
+			if(e.getDamager() instanceof Projectile) {
+				if(((Projectile) e.getDamager()).getShooter() instanceof Player){
+					damager = (Player)(((Projectile)e.getDamager()).getShooter());
+				} else {
+					return;
+				}
+			} else if(e.getDamager() instanceof Player){
+				damager = (Player)e.getDamager();
+			} else {
+				return;
 			}
+
+			e.setDamage(e.getDamage() - hitBoss.getStats().getDefense());
+			Double dmg = hitBoss.getPlayerDamage().get(e.getDamager().getUniqueId());
+			if(dmg == null) dmg = 0.0;
+			hitBoss.getPlayerDamage().put(damager.getUniqueId(), dmg + e.getDamage());
+			hitBoss.setNameAndHealth();
 		}
 	}
-	
+
 	@EventHandler
 	public void onDeath(EntityDeathEvent e) {
 		BossEntity hitBoss = BossManager.getCurrBosses().get(e.getEntity().getUniqueId());
@@ -36,7 +50,7 @@ public class BossDamageListener implements Listener{
 			hitBoss.despawn();
 		}
 	}
-	
+
 	@EventHandler
 	public void onRegen(EntityRegainHealthEvent e) {
 		BossEntity hitBoss = BossManager.getCurrBosses().get(e.getEntity().getUniqueId());
@@ -44,7 +58,7 @@ public class BossDamageListener implements Listener{
 			hitBoss.setNameAndHealth();
 		}
 	}
-	
+
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent e) {
 		for(BossEntity boss : BossManager.getCurrBosses().values()) {
